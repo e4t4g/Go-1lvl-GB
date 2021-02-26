a333fb7d7b728f6e4838de4b792e9cda1b280694
e4t4g <evahillertyan@gmail.com>
2021-02-26 18:00:06 +0300

    3 lesson

---
+++ calc2.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+	var sign string
+	var a float64
+
+	fmt.Print("Enter the first number: ")
+	a, err := vali()
+	if err != nil {
+		fmt.Println("error NaN")
+		return
+
+	}
+
+	fmt.Print("Enter one of the signs (+ - * /): ")
+	fmt.Scan(&sign)
+
+	fmt.Print("Enter the second number: ")
+	b, err := vali()
+	if err != nil {
+		fmt.Println("error NaN")
+		return
+	}
+
+	switch sign {
+	case "+":
+		fmt.Printf("Answer: %0.2f", a+b)
+	case "-":
+		fmt.Printf("Answer: %0.2f", a-b)
+	case "*":
+		fmt.Printf("Answer: %0.2f", a*b)
+	case "/":
+		if b == 0 {
+			fmt.Println("Division by zero.")
+			break
+		} else {
+			fmt.Printf("Answer: %0.2f", a/b)
+		}
+
+	default:
+		fmt.Println("Wrong")
+	}
+
+}
+
+func vali() (float64, error) {
+	var x string
+	fmt.Scan(&x)
+	y, err := strconv.ParseFloat(x,64)
+	if err!=nil {
+		 return y, errors.New("not empty error")
+	} else {
+		return y, nil
+	}
+}
